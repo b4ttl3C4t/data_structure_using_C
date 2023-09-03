f885518419ee869d63be5daba3536370a83a3e55
b4ttl3C4t <129575894+b4ttl3C4t@users.noreply.github.com>
2023-09-03 20:13:59 +0800

    concept of math

---
+++ matrix.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MATRIX_SIZE 10
+
+//function prototype for operation of matrix:
+void    traverse(int, int *[]);
+int *   scalar_product(int, int *, int);
+int **  vector_product(int, int (*)[], int);
+int **  transpose();
+int **  UV_decomposition();
+int **  determinant();
+int **  inverse();
+
+int main(void)
+{
+    
+    return 0;
+}
