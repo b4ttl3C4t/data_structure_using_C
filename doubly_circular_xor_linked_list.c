e1cc625eb273de6301ca070edd0aba9d063a7719
b4ttl3C4t <sam00298653@gmail.com>
2023-10-12 11:08:28 +0800

    test

---
+++ doubly_circular_xor_linked_list.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "circular_double_xor_linked_list.h"
+
+#define LIST_SIZE       50
+#define LIST_TYPE_SIZE  2
+#define OPCODE_SIZE     14
+#define MALLOC_ERROR    "The memory allocation for linked list aborts."
+#define MODE_ERROR      "The mode is not accessible."
+#define OPCODE_ERROR    "The opcode is not accessible."
+#define INDEX_ERROR     "The node does not exist."
+
+int main(void)
+{
+    l_List *list = (l_List *)malloc(sizeof(l_List));
+    uint64_t opcode;
+
+    while(1)
+    {
+        printf("\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s",
+                            "> control_table: ",
+                            "  |code|    function    |",
+                            "  |  0 | construction   |",
+                            "  |  1 | insertion      |",
+                            "  |  2 | insert_head    |",
+                            "  |  3 | insert_tail    |",
+                            "  |  4 | destruction    |",
+                            "  |  5 | deletion       |",
+                            "  |  6 | delete_head    |",
+                            "  |  7 | delete_tail    |",
+                            "  |  8 | search         |",
+                            "  |  9 | display        |",
+                            "  | 10 | sort           |",
+                            "  | 11 | reverse        |",
+                            "  | 12 | is_list_empty  |",
+                            "  | 13 |      EXIT      |",
+                            "> control_table: What do you want to do now? Please enter the opcode:");
+        scanf("%llu", &opcode);
+        getchar();
+
+        if(opcode >= OPCODE_SIZE)
+        {
+            fprintf(stderr, "\n> control_table: %s", OPCODE_ERROR);
+        }
+
+        l_control_table(&list, opcode);
+    }
+}
+
+void l_control_table(l_List **list, uint64_t opcode)
+{
+    uint64_t index;
+
+    switch(opcode)
+    {
+    case 0:
+        break;
+
+    case 3:
+        break;
+
+    case 7:
+        break;
+
+    case 10:
+        break;
+    }
+
+    return;
+}
+
+static void input_data  (l_Node *node);
+static void swap        (l_Node *X, l_Node *Y);
+static l_Node * node_xor(l_Node *X, l_Node *Y);
+
+/*----------------------------------------function *l_initialization*  begin----------------------------------------*/
+l_List * l_initialization(void)
+{
+    printf("\n%s", "> Initialization: Allocating the memory space for head node.");
+    l_List *list = (l_List *)calloc(1, sizeof(l_List));
+
+    //Probing whether the memory allocation successes.
+    if(list == NULL)
+    {
+        fprintf(stderr, "\n%s%s", "> Initialization: ", MALLOC_ERROR);
+        return NULL;
+    }
+
+    printf("\n%s", "> Initialization: The memory allocation completes!");
+    printf("\n%s\n%s\n%s",  "> Initialization: Type mode table as following:",
+                                    "  | 0 | normal linked list     |",
+                                    "  | 1 | polynomial linked list |");
+    
+    //Interface for setting mode.
+    printf("\n%s", "> Initialization: Please set the type mode for the head node:");
+    scanf("%llu", list->type);
+    getchar();
+    
+    //Probing whether the type mode is correct.
+    if(list->type >= LIST_TYPE_SIZE)
+    {
+        fprintf(stderr, "\n%s%s", "> Initialization: ", MODE_ERROR);
+        return NULL;
+    }
+    return list;
+}
+/*----------------------------------------function *l_initialization* end  ----------------------------------------*/
---
+++ doubly_circular_xor_linked_list.h
@@ -0,0 +1,92 @@
+#ifndef CIRCULAR_DOUBLE_XOR_LINKED_LIST_H
+#define CIRCULAR_DOUBLE_XOR_LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+/*You should set the mode for the newer node (as following)
+ *whenever you construst it.
+ */
+enum LinkedListType
+{
+    normal      = 0,
+    polynomial  = 1,
+};
+
+//The definition of data for polynomials.
+struct polynomial_data
+{
+    double coefficient;
+    uint64_t power;
+};
+
+//The definition of data for the normal node.
+struct node_data
+{
+    uint64_t integer;
+};
+
+//Constructing the data of compound linked list type.
+typedef struct LINKED_LIST_DATA
+{
+    union
+    {
+        struct polynomial_data polynomial;
+        struct node_data data;
+    };
+} l_Data;
+
+/*The node type is for doubly circular xor linked list.*/
+typedef struct LINKED_LIST_NODE
+{
+    struct LINKED_LIST_DATA *data;
+    struct LINKED_LIST_NODE *link;
+} l_Node;
+/*The pointer to data is used to promote the code flexiblity, 
+ *when you swap the data within two node, or change the data setting.
+ */
+
+//The linked list type
+typedef struct LINKED_LIST_NODE
+{
+    enum LinkedListType type;
+    l_Node *head_node;
+    l_Node *sentinel_node;
+    l_Node *dummy_node;
+} l_List;
+//Constructing compound linked list type, then setting the mode by variable *type* .
+//The *head node* means the beginning of the linked list.
+//The *sentinel node* means the check point of the linked list.
+/*The *dummy node* can avoid the special operation to the head node.
+ *and let the code more clear and promote its readness.
+ */
+
+//Function prototype for operation of linked list:
+void l_control_table    (l_List **, uint64_t);
+
+void l_construction     (l_List *);
+//void l_insertion        (l_List *, uint64_t);
+//void l_insert_head      (l_List *);
+//void l_insert_tail      (l_List *);
+
+//void l_destruction      (l_List *);
+//void l_deletion         (l_List *, uint64_t);
+//void l_delete_head      (l_List *);
+//void l_delete_tail      (l_List *);
+
+//Useful operation and algorithm based on divergent linked list type.
+//void l_search           (l_List *, uint64_t);
+//void l_display          (l_List *);
+//void l_sort             (l_List *);
+//void l_reverse          (l_List *);
+//bool l_is_list_empty    (l_List *);
+/*
+//The following algorithm is only for polynomial linked list.
+l_List * polynomial_addition          (l_List *, l_List *);
+l_List * polynomial_subtraction       (l_List *, l_List *);
+l_List * polynomial_multiplication    (l_List *, l_List *);
+l_List * polynomial_division          (l_List *, l_List *);*/
+
+#endif
